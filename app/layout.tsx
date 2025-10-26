7a2bb3d78664f6a7e6d813af06ad67275be7e368
AmitStredz <ponnan2112@gmail.com>
2025-10-27 01:29:55 +0530

    css updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,43 @@
-// import type { Metadata } from "next";
-// import { GeistSans } from "geist/font/sans";
-// import { GeistMono } from "geist/font/mono";
-// import "./globals.css";
-// import { ClerkProvider } from "@clerk/nextjs";
-// import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
 
-// export const metadata: Metadata = {
-//   title: "ChatGPT",
-//   description: "Created with v0",
-//   generator: "v0.dev",
-//   icons: {
-//     icon: "/gpt-logo.svg",
-//   }
-// };
+export const metadata: Metadata = {
+  title: "ChatGPT",
+  description: "Created with v0",
+  generator: "v0.dev",
+  icons: {
+    icon: "/gpt-logo.svg",
+  }
+};
 
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   const publishableKey =
-//     process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ||
-//     process.env.CLERK_PUBLISHABLE_KEY;
-//   return (
-//     <ClerkProvider publishableKey={publishableKey}>
-//       <html lang="en" suppressHydrationWarning>
-//         <head>
-//           <style>{`
-// html {
-//   font-family: ${GeistSans.style.fontFamily}, sans-serif;
-// }
-//           `}</style>
-//         </head>
-//         <body>
-//           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-//             {children}
-//           </ThemeProvider>
-//         </body>
-//       </html>
-//     </ClerkProvider>
-//   );
-// }
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const publishableKey =
+    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ||
+    process.env.CLERK_PUBLISHABLE_KEY;
+  return (
+    <ClerkProvider publishableKey={publishableKey}>
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <style>{`
+html {
+  font-family: ${GeistSans.style.fontFamily}, sans-serif;
+}
+          `}</style>
+        </head>
+        <body>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
